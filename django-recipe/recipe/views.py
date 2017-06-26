412e6a6b61eaf1958b749e78f1703500524ba53d
Greg Benton <greg.w.benton@gmail.com>
2017-06-26 11:57:52 -0600

    Add files via upload

--- django-recipe/recipe/views.py
+++ django-recipe/recipe/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from recipe.models import Ingredients, Recipes, RecipeIngredients
 
 
 # from .forms import NameForm
@@ -10,7 +11,11 @@ def get_input(request):
         search_id = request.GET.get('text_input', None) #text_input is from line 42 in html
         if search_id is not None:
             search_id = search_id.split()
-    return render(request, 'index.html', {'user_input': search_id}) #goes to the html page
+
+    ingredient_key = 14
+    ingredient_name = Ingredients.objects.get(id = ingredient_key).name
+
+    return render(request, 'index.html', {'user_input': ingredient_name}) #goes to the html page
 
     #print "Your input:", search_id --check if input is correct
     return render(request, 'index.html') #goes to the html page

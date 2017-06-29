921dfce7662e3eb6bd712e3c88aabc42b88b9003
Erin Ruby <erinruby@engr2-14-4-dhcp.int.colorado.edu>
2017-06-28 21:59:36 -0600

    higreg

--- app/search/views.py
+++ app/search/views.py
@@ -20,5 +20,8 @@ def search_recipes(request, search_id):
         # return recipe_views.get_input(request)
         return render(request, 'index.html')
     test = get_urls.get_urls(recipe_results)
+    if(request.GET.get('reset')):
+        return render(request, 'index.html')
+
     return render(request, 'base.html', {'data' : test})
     # return render_template('index.html',name=recipe_results)

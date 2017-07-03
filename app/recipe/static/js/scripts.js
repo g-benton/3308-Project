ae26cf34a622db75d73c53f278d69c5d62808613
Erin Ruby <erinruby@engr2-15-191-dhcp.int.colorado.edu>
2017-07-03 11:08:46 -0600

    updated index.html, style.css and scripts.js

--- app/recipe/static/js/scripts.js
+++ app/recipe/static/js/scripts.js
@@ -1,37 +1,14 @@
-// Get the modal
-window.onload = function() {
-   var modal = document.getElementById('myModal');
-   // Get the button that opens the modal
-   var btn = document.getElementById("myBtn");
-   // var btn = document.getElementsByClassName("flexsearch--submit");
-   // Get the <span> element that closes the modal
-   var exit = document.getElementsByClassName("close");
-   var exitno = document.getElementById("no");
-   var next = document.getElementById("ya");
-   // When the user clicks on the button, open the modal
-   btn.onclick = function() {
-      modal.style.display = "block";
-   }
+var sheepHead = document.getElementById("sheep-eating-head");
 
-   // When the user clicks on (x), close the modal
-   exit.onclick = function()
-   {
-      modal.style.display = "none";
-   }
-   exitno.onclick = function()
-   {
-      modal.style.display = "none";
-   }
-   next.onclick = function()
-   {
-      modal.location.reload(true);
-   }
-   // When the user clicks anywhere outside of the modal, close it
-   window.onclick = function(event)
-   {
-      if (event.target == modal)
-      {
-           modal.style.display = "none";
-      }
-   }
-}
+TweenMax.to(
+  sheepHead,
+  4.8,
+  {
+    y: '20px',
+    rotation: -40,
+    transformOrigin: 'center center',
+    yoyo: true,
+    ease: Power1.easeInOut,
+    repeat: -1
+  }
+)
